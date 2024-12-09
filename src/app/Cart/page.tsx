32960d09e835f23ae7cfd8f4413feffb2f8add45
wish Malik <malik.mehwish0078@gmail.com>
2024-12-09 19:56:58 +0500

    first commit

--- src/app/Cart/page.tsx
+++ src/app/Cart/page.tsx
@@ -3,7 +3,7 @@
 import React from 'react'
 import Image from "next/image"
 import Link from "next/link"
-import Frame from "@/app/assets/Frame (34).png"
+import Frame from "@/app/Assets/Frame (34).png"
 import { FaRegHeart } from "react-icons/fa6";
 import { RiDeleteBin6Line } from "react-icons/ri";
 const Cart = () => {
