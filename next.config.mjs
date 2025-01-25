1a546483e905004ff5ceab2614fd2cc87da3ac71
wish Malik <malik.mehwish0078@gmail.com>
2025-01-25 05:14:56 +0500

    added sanity

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains:["cdn.sanity.io"],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cdn.sanity.io',
+            port: '',
+            pathname: '//template-03-api.vercel.app/api/products/**', // Matches all image paths from Sanity
+          },
+        ],
+      },
     }
-};
 
 export default nextConfig;
